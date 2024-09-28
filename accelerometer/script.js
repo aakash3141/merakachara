e7d667e9ac1b2b44d2b048e66b5064ff0acfe800
aakash3141 <macroqore@gmail.com>
2024-09-28 17:05:52 -0400

    Update script.js

--- accelerometer/script.js
+++ accelerometer/script.js
@@ -1,1 +1,26 @@
+var num = 0;
+function addangle()
+{
+    if(num + 10 <= 150)
+    {
+      num += 10;
+    }
+    else
+    {
+      num = 150;
+    }
+    document.getElementById("needle").style.transform = "rotate(" + num + "deg)";
+}
 
+function subtractangle()
+{
+    if(num - 10 >= -150)
+    {
+     	num -= 10;
+    }
+    else
+    {
+     	num = -150;
+    }
+    document.getElementById("needle").style.transform = "rotate(" + num + "deg)";
+}
